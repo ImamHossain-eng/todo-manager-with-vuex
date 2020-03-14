c4075bfd12287ccc968c2f2ec296e65de4033b68
Imam Khan <babubpx2762@gmail.com>
2020-03-14 22:02:07 +0600

    adding and removing functionality added to the project

---
+++ src/components/AddTodos.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <div id="form">
+            <form @submit="onSubmit">
+                <input type="text" v-model="title" placeholder="Add Todo.....">
+                <input type="submit" value="Submit">
+            </form>
+
+        </div>
+    </div>
+</template>
+<script>
+import { mapActions } from 'vuex'
+export default {
+    Name: "AddTodo",
+    data() {
+        return {
+            title: ''
+        }
+    },
+    methods: {
+        ...mapActions(['addTodo']),
+        onSubmit(e){
+            e.preventDefault();
+            this.addTodo(this.title);
+            this.title ='';
+        }
+    }
+}
+</script>
+<style scoped>
+#form{
+    display: flex;
+    width: 100%;
+}
+input[type="text"]{
+    flex: 10;
+    border: 1px solid #262626;
+    padding: .7rem;
+    outline: 0;
+}
+input[type="submit"]{
+    flex:2;
+    color: #efefef;
+    background: #262626;
+    padding: .7rem;
+    cursor: pointer;
+}
+input[type="submit"]:hover{
+    color:#262626;
+    background:#fff
+}
+
+
+</style>
--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -3,7 +3,9 @@
         <h1 class="header">Todos List</h1>
         <div class="todos">
             <div v-for="todo in allTodos" :key="todo.id" class="todo">
-                <h4>{{todo.title}}</h4>
+                <h4>{{todo.title}}
+                    <i @click="deleteTodo(todo.id)" class="fa fa-trash"></i>
+                </h4>
             </div>
         </div>
     </div>
@@ -14,7 +16,7 @@ import { mapGetters, mapActions } from 'vuex';
 export default {
     name: "Todos",
     methods: {
-        ...mapActions(['fetchTodos'])
+        ...mapActions(['fetchTodos', 'deleteTodo'])
     },
     computed: mapGetters(['allTodos']),
     created() {
@@ -38,8 +40,13 @@ export default {
     padding: 1rem;
     text-align: center;
     position: relative;
+}
+i{
+    position: absolute;
+    bottom: 10px;
+    right: 10px;
+    color: #fff;
     cursor: pointer;
-
 }
 
 </style>
